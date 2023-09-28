5396fadffbdaadf61fed4ee920d719422d19d496
Eben <maradebenezer@gmail.com>
2023-09-28 03:34:25 +0100

    added check for n less than or equal to 0

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,7 +4,12 @@
 
 def pascal_triangle(n):
     """ pascal's triangle function """
+    
     grand_list = []
+    
+    if (n <= 0):
+        return (grand_list)
+
     for i in range(n):
         grand_list.append(str(11**i))
     return (grand_list)
