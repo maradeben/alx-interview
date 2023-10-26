471694d7b64ec2c823fd23d61e3070809d74f2b5
Eben <maradebenezer@gmail.com>
2023-10-26 16:44:04 +0100

    fixing 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -9,8 +9,7 @@ def validUTF8(data: List) -> bool:
 
     for byte in data:
         byte = bin(byte)[2:].zfill(8)  # convert to binary, 8 bits
-
-        if following_bytes == '0':
+        if following_bytes == 0:
             if byte[0] == '0':
                 continue
             elif byte.startswith('110'):
@@ -19,7 +18,7 @@ def validUTF8(data: List) -> bool:
                 following_bytes = 2
             elif byte.startswith('11110'):
                 following_bytes = 3
-            else:
+            elif binary.startswith('10'):
                 return False
         else:
             if not byte.startswith('10'):
