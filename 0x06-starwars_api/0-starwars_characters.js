05cbd5f0043ed405fb9dd55a9cf4975833b262e4
Eben <maradebenezer@gmail.com>
2023-11-10 17:16:56 +0100

    fixing 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,53 +1,24 @@
-#!/usr/bin/node
+const fetch = require('node-fetch');
 
-const request = require('request');
+// Get the movie ID from the first positional argument
+const movieId = process.argv[1];
 
-const movieId = process.argv[2];
-const filmEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
-let people = [];
-const names = [];
+// Make a request to the Star Wars API to get the movie data
+async function fetchStarWarsMovieData(movieId) {
+  const response = await fetch(`https://swapi.dev/api/films/${movieId}`);
+  const movieData = await response.json();
+  return movieData;
+}
 
-const requestCharacters = async () => {
-  await new Promise(resolve => request(filmEndPoint, (err, res, body) => {
-    if (err || res.statusCode !== 200) {
-      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
-    } else {
-      const jsonBody = JSON.parse(body);
-      people = jsonBody.characters;
-      resolve();
-    }
-  }));
-};
+// Print each character name on a new line
+async function printStarWarsCharacterNames(movieId) {
+  const movieData = await fetchStarWarsMovieData(movieId);
+  const characters = movieData.characters;
 
-const requestNames = async () => {
-  if (people.length > 0) {
-    for (const p of people) {
-      await new Promise(resolve => request(p, (err, res, body) => {
-        if (err || res.statusCode !== 200) {
-          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
-        } else {
-          const jsonBody = JSON.parse(body);
-          names.push(jsonBody.name);
-          resolve();
-        }
-      }));
-    }
-  } else {
-    console.error('No characters returned');
+  for (const character of characters) {
+    console.log(character);
   }
-};
+}
 
-const getCharNames = async () => {
-  await requestCharacters();
-  await requestNames();
-
-  for (const n of names) {
-    if (n === names[names.length - 1]) {
-      process.stdout.write(n);
-    } else {
-      process.stdout.write(n + '\n');
-    }
-  }
-};
-
-getCharNames();
+// Call the printStarWarsCharacterNames function with the movie ID
+printStarWarsCharacterNames(movieId);
