62d60a390dcd351cf35132cccf5534d44091b5f6
Eben <maradebenezer@gmail.com>
2023-11-15 22:51:04 +0100

    corrected output order

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,7 +4,7 @@
 
 def rotate_2d_matrix(matrix):
     """ rotate the 2d matrix in place, do not return anything """
-    
+
     if type(matrix) != list or len(matrix) <= 0:
         return
     if not all(map(lambda x: type(x) == list, matrix)):
@@ -16,4 +16,5 @@ def rotate_2d_matrix(matrix):
             new[j][i] = matrix[i][j]
 
     for i in range(size):
+        new[i].reverse()
         matrix[i] = new[i]
