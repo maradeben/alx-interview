60a2ed681cd4c58aa6b402c3ca281718e54a9277
Eben <maradebenezer@gmail.com>
2023-11-15 21:17:26 +0100

    added checks

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,7 +4,11 @@
 
 def rotate_2d_matrix(matrix):
     """ rotate the 2d matrix in place, do not return anything """
-
+    
+    if type(matrix) != list or len(matrix) <= 0:
+        return
+    if not all(map(lambda x: type(x) == list, matrix)):
+        return
     size = len((matrix))
     new = [[0 for i in range(size)] for i in range(size)]
     for i in range(size):
