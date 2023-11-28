17215a9148bb7879f08ccb9f2a813dcaf7e6aa00
Eben <maradebenezer@gmail.com>
2023-11-28 15:01:10 +0100

    corrections

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -2,8 +2,8 @@
 """ island perimeter algorithm """
 
 
-def check_around(grid, i, j):
-    """ check surroundings of a cell """
+"""def check_around(grid, i, j):
+    "" check surroundings of a cell ""
     perim = 0
 
     if grid[i][j-1] == 0:
@@ -15,6 +15,7 @@ def check_around(grid, i, j):
     if grid[i+1][j] == 0:
         perim += 1
     return perim
+"""
 
 
 def island_perimeter(grid):
@@ -24,6 +25,15 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
-                perimeter += check_around(grid, i, j)
+                """perimeter += check_around(grid, i, j)"""
+                perimeter += 4
+
+                # check left neighbor
+                if j > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+
+                # check left neighbor
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
 
     return perimeter
